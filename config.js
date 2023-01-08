3cc0333fd8443183a2632bff4dc5929026d25d2f
lashmwon <111589930+lashmwon@users.noreply.github.com>
2023-01-08 18:55:02 +0530

    Update config.js

--- config.js
+++ config.js
@@ -10,14 +10,14 @@ global.antitags = true
 //global.autoTyping = false //auto tying in gc (true to on, false to off)
 //global.autoRecord = true //auto recording (true to on, false to off)
 
-global.Owner = ['918348225320'] //ur number 
+global.Owner = ['918714310038'] //ur number 
 global.OwnerNumber = ['918348225320']
 global.ownertag = ['918348225320']
-global.BotName = "ZenitsuBot" //ur bot name 
+global.BotName = "Zeus" //ur bot name 
 global.packname = "ZenitsuBot"
-global.author = "🦄Dream Guy Deepak" //ur sticker pack name 
+global.author = "🦄zeus" //ur sticker pack name 
 global.OwnerName = "🦄Dream Guy Deepak" //ur name
-global.BotSourceCode = "https://youtube.com/@dreamguydeepak" //ur website link
+global.BotSourceCode = "https://www.instagram.com/lxsshh/" //ur website link
 global.SupportGroupLink = "https://chat.whatsapp.com/KguOo9XsRTW2iESYuHzVX1" //ur group link 
 global.sessionName = "session"
 
